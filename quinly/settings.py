eef9cfdbeff7e93f799ccdbef44055679047b1fb
Bisma Bashir <bismabashir7268@gmail.com>
2025-03-24 11:06:36 +0500

    final commit

--- quinly/settings.py
+++ quinly/settings.py
@@ -58,6 +58,7 @@ CHANNEL_LAYERS = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
