224c20a4a6935c73af0956e373a1441b101d0241
Bisma Bashir <bismabashir7268@gmail.com>
2025-03-24 16:40:02 +0500

    final commit

--- quinly/asgi.py
+++ quinly/asgi.py
@@ -7,16 +7,37 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
-import os
+# import os
 
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
+# from django.core.asgi import get_asgi_application
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# import django
+# from channels.auth import AuthMiddlewareStack
+# from chat.routing import websocket_urlpatterns
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quinly.settings')
+# django.setup()
+
+# application = ProtocolTypeRouter({
+#     "http": get_asgi_application(),
+#     "websocket": AuthMiddlewareStack(
+#         URLRouter(websocket_urlpatterns)
+#     ),
+# })
+
+import os
 import django
+from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from chat.routing import websocket_urlpatterns
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quinly.settings')
+
+# Setup Django BEFORE importing anything that uses models or settings
 django.setup()
+
+# Now safe to import after setup
+from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
